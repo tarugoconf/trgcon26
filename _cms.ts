dce20d75dfcdedbf157fc842aff7e993bf13a020
bonilla <>
2026-03-17 19:18:55 +0000

    Changes from CMS

--- _cms.ts
+++ _cms.ts
@@ -1,3 +1,12 @@
 import cms from "top/_cms.ts";
 
 export default cms;
+
+const user = Deno.env.get("CMS_USER") ?? "bonilla";
+const pass = Deno.env.get("CMS_PASSWORD") ?? "bonilla";
+
+cms.auth({
+  [user]: pass,
+});
+
+cms.git();
